4ae6a15d9c15e7cea799095feb94450080398488
gaurav1620 <gauravak007@gmail.com>
2020-06-24 21:57:13 +0530

    Fib not giving TLE

--- Test.py
+++ Test.py
@@ -4,6 +4,9 @@
 #https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
 #hackerrank war aahe ha problem , tyachat n = 30 max ahe ani to fit hoto
 #karan 2 power 30 = 1073741824 which is close to 10 power 8
+
+
+#this gives TLE
 def fib(f,s,n):
     if n == 1:
         return f
