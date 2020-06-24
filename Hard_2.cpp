0414ca1a054b9c972b2a497c8c67450621073238
gaurav1620 <gauravak007@gmail.com>
2020-06-24 12:14:28 +0530

    Added hard 2

---
+++ Hard_2.cpp
@@ -0,0 +1,47 @@
+/* Author     : Gaurav Khairnar
+ * Codechef   : gaurav_1620
+ * Codeforces : gaurav1620
+ * Mail       : gauravak007@gmail.com
+ */
+
+#include<bits/stdc++.h>
+
+#define ll long long
+#define FOR(i,j,k) for(i = j;i <= k;i++)
+using namespace std;
+
+//*
+
+Problem statement
+
+    There are n kattas in pvg (1,2,3,4.... n).
+    There are also m roads in the format x,y that connect
+    katta x and katta y.
+    Also you have been given an array of n distinct numbers for which 
+    the i'th element represents the beauty of the i'th katta
+
+    There are n students admitted in PVG
+    and as the CORONA pandemic is still going on each one of 
+    them sits on a different katta. After the pandemic is over
+    all the studnets try to explore new katttas that they can
+    go to.
+
+    NAAC is going to come to PVG to asses the grade which is 
+    an integer. Interestingly NAAC computes the grade in the 
+    following process : 
+        NAAC asks each student the maximum beautiful katta 
+        that he had seen till now and adds that students answer 
+        to the database.
+        After that NAAC pulls out all the unique values
+        from this database and finds the summation 
+        of these values to get the grade.
+
+    You are the NAAC officer assignes to find the grade of PVG.
+    Print a single integer which will be the grade of PVG.
+
+*/
+
+int main(){
+    ll c;cin>>c;
+    return 0;
+}
